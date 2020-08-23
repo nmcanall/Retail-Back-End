ebe77cff397ceb95da50fef4668abe3347e53883
nmcanall <nmcanall@citadel.edu>
2020-08-22 18:53:33 -0700

    build all models and relationships

--- models/index.js
+++ models/index.js
@@ -5,11 +5,27 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: "product-tag_relationship",
+    foreignKey: "product_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: "product-tag_relationship",
+    foreignKey: "tag_id"
+});
 
 module.exports = {Product, Category, Tag, ProductTag};
